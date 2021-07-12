535420518d63326ec87099f5745653b8b85c5122
IA Masum <38923620+IA-Masum@users.noreply.github.com>
2021-07-12 22:41:53 +0600

    Jquery 1st Class
    Demo Descripton

---
+++ JQuery/main.js
@@ -0,0 +1,71 @@
+
+
+$(document).ready(function(){
+
+
+  // $('#jq').click(function (){
+
+  //   console.log('h1 Clicked');
+  // });
+
+  // $('h1').click(function (){
+
+  //   // console.log($(this).text());
+  //   $(this).text("Clicked");
+
+  // });
+  // $('h1').on({
+  //   click : function () {
+  //     $(this).text("Clicked")
+  //   },
+
+  //   mouseenter: function () {
+  //     $(this).text("Hovered")
+  //   }
+  // });
+
+  // $('#input').keypress(function (e) {
+
+  //   $('#out').text($(this).val())
+
+  // })
+
+  // $('#h').click(function(){
+  //   $('p').hide();
+  // })
+  // $('#s').click(function(){
+  //   $('p').show();
+  // })
+  // $('#t').click(function(){
+  //   $('p').toggle();
+  // })
+  // $('#h').click(function(){
+  //   $('p').fadeOut(3000, function(){
+  //     alert("Element is vanished.")
+  //   });
+  // })
+  // $('#s').click(function(){
+  //   $('p').fadeIn(3000, function(){
+  //     alert("Element is appeared.")
+  //   });
+  // })
+  // $('#t').click(function(){
+  //   $('p').fadeToggle(3000, function(){
+  //     alert("Element is toggled.")
+  //   });
+  // })
+  $('#h').click(function(){
+    $('p').slideUp(3000).slideDown(3000).css({
+      backgroundColor: 'red'
+    });
+  })
+  // $('#s').click(function(){
+  //   $('p').slideDown(3000);
+  // })
+  // $('#t').click(function(){
+  //   $('p').slideToggle(3000);
+  // })
+
+
+
+});
