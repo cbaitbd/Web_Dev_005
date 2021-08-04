e2d52110e0123e39abd8f8b533c539f33f686c4f
IA Masum <38923620+IA-Masum@users.noreply.github.com>
2021-08-04 15:02:33 +0600

    discuss abut Array and loop

--- basic_php/basic.php
+++ basic_php/basic.php
@@ -2,12 +2,36 @@
 
 <?php 
 
-  $first_name = "Imran";
-  $last_name = "Ali";
+  // $arr = [1, 2, 3, 4];
 
-  $full_name = $first_name." ".$last_name;
+  $assoc = [
+    'name' => 'Mr. Abdul Goni',
+    'age' => 50,
+    'address' => "Baneswar, Rajshahi"
+  ];
 
-  echo $full_name;
+  $assoc['phone'] = 21556987441;
+
+  // foreach ($assoc as $key => $value) {
+  //   echo $key." : ".$value;
+  //   echo "<br>";
+  // }
+
+
+
+  // print_r($assoc);
+  // var_dump($arr);
+
+
+  $arr = [10, 100, 2, 98, 41, 21, 500, 0, 6];
+  $names = ['momin', 'arafat', 'rasel', 'maruf', 'shohel', 'imran', 'mustafiz', 'alomgir'];
+  rsort($arr);
+  rsort($names);
+
+  krsort($assoc);
+
+  print_r($assoc);
+  
 
 
 ?>
